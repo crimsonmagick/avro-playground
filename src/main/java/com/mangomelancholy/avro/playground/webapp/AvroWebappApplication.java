4aa8ec1bfe9e11df2c625ed654e985bdd62e7411
Welby Seely <welbyseely@gmail.com>
2023-05-22 17:30:11 -0400

    Initialize codebase

---
+++ src/main/java/com/mangomelancholy/avro/playground/webapp/AvroWebappApplication.java
@@ -0,0 +1,13 @@
+package com.mangomelancholy.avro.playground.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvroWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvroWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangomelancholy/avro/playground/webapp/AvroWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangomelancholy.avro.playground.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
